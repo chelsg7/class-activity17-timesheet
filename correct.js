0725a9ff5cb81984b754fd4da1b85db2611c50c5
Edward Nimocks <enimocks@gmail.com>
2018-04-02 19:37:58 -0400

    fixed old push

--- correct.js
+++ correct.js
@@ -9,59 +9,31 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var dataRef = firebase.database();
-
-// Initial Values
-var name = "";
-var email = "";
-var age = 0;
-var comment = "";
-
-// Capture Button Click
-$("#add-user").on("click", function(event) {
+//Button for adding Employees
+$("#add-employee-btn").on("click", function(event) {
   event.preventDefault();
-  // YOUR TASK!!!
-  // Code in the logic for storing and retrieving the most recent user.
-  // Don't forget to provide initial data to your Firebase database.
-  name = $("#name-input").val().trim();
-  email = $("#email-input").val().trim();
-  age = $("#age-input").val().trim();
-  comment = $("#comment-input").val().trim();
 
-  // Code for the push
-  dataRef.ref().push({
-    name: name,
-    email: email,
-    age: age,
-    comment: comment,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+  // Grabs user input
+  var empName = $("#employee-name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+
+  // Clears all of the text-boxes
+  $("#employee-name-input").val("");
+  $("#role-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+
+  // Add each train's data into the table
+  $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+  empStart + "</td><td>" + "null" + "</td><td>" + empRate + "</td><td>" + "null" + "</td></tr>");
+
+  database.ref().push({
+    empName: empName,
+    empRole: empRole,
+    empStart: firebase.database.ServerValue.TIMESTAMP,
+    empRate: empRate
+  })
 });
 
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-dataRef.ref().on("child_added", function(childSnapshot) {
-  // Log everything that's coming out of snapshot
-  console.log(childSnapshot.val());
-  console.log(childSnapshot.val().name);
-  console.log(childSnapshot.val().email);
-  console.log(childSnapshot.val().age);
-  console.log(childSnapshot.val().comment);
-  console.log(childSnapshot.val().dateAdded);
-
-  // full list of items to the well
-  $("#full-member-list").append("<div class='well'><span class='member-name'> " + childSnapshot.val().name +
-    " </span><span class='member-email'> " + childSnapshot.val().email +
-    " </span><span class='member-age'> " + childSnapshot.val().age +
-    " </span><span class='member-comment'> " + childSnapshot.val().comment + " </span></div>");
-
-// Handle the errors
-}, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
-});
-dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-  // Change the HTML to reflect
-  $("#name-display").text(snapshot.val().name);
-  $("#email-display").text(snapshot.val().email);
-  $("#age-display").text(snapshot.val().age);
-  $("#comment-display").text(snapshot.val().comment);
-});
